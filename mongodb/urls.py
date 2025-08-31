51e4c45784876abd812880a5e440a6b2553387c6
evgenij <ef@efsoft.de>
2025-09-01 01:42:40 +0200

    refactoring

--- mongodb/urls.py
+++ mongodb/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create-database/step1/', views.create_database_step1, name='create_database_step1'),
-    path('create-database/step2/', views.create_database_step2, name='create_database_step2'),
-    path('create-database/step3/', views.create_database_step3, name='create_database_step3'),
+    path('create/step1/', views.create_database_step1, name='create_database_step1'),
+    path('create/step2/', views.create_database_step2, name='create_database_step2'),
+    path('create/step3/', views.create_database_step3, name='create_database_step3'),
 ]
